de998b590715e8bdd997b700dfa0de0bce25a211
Porcupinee <htolley0@gmail.com>
2023-05-19 17:18:48 +1000

    Contact me and resume page finalised and styled.

--- js/projects.js
+++ js/projects.js
@@ -1,13 +1,21 @@
-document.addEventListener("DOMContentLoaded", function () {
-
-    var splide = new Splide('.splide', {
-        type   : 'loop',
-        drag   : 'free',
-        snap   : true,
-        perPage: 3,
-      });
-    
-      splide.mount();
-
-    });
-
+document.addEventListener('DOMContentLoaded', function () {
+    new Splide('.splide', {
+      type: 'loop',
+      perPage: 3,
+      perMove: 1,
+      focus: 'center',
+      pagination: false,
+      fixedWidth: '30%',  
+      breakpoints: {
+        1600: {
+            perPage: 1,
+            fixedWidth: '60%',
+        },
+        600: {
+          perPage: 1,
+          fixedWidth: '80%',
+        },
+      },
+    }).mount();
+  });
+  
